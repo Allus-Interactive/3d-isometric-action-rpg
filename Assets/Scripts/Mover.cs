644e6f029783f605eb7065646719b3e53511e01e
Reece <allus.interactive@gmail.com>
2022-10-09 22:20:14 +0100

    implemented basic click to move logic

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,15 +7,22 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    Ray lastRay;
-
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+            MoveToCursor();
         }
-        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    private void MoveToCursor() 
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit) 
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
