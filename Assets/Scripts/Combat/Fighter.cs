e182c7ced4d74812cb1be11beb4d01e4e75eaa26
Reece <reecemorgandev@gmail.com>
2023-01-05 13:02:47 +0000

    fix first attack bug

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,7 +10,7 @@ namespace RPG.Combat
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
 
-        float timeSinceLastAttack = 0;
+        float timeSinceLastAttack = Mathf.Infinity;
 
         Health target;
 
