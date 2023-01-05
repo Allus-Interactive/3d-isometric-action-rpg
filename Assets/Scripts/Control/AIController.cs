7366f647dcb29cc039abd81c0c4c58806301ff56
Reece <reecemorgandev@gmail.com>
2023-01-05 14:20:16 +0000

    improved guarding behaviour

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -8,16 +8,20 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
 
+        ActionScheduler actionScheduler;
         Fighter fighter;
         Health health;
         Mover mover;
         GameObject player;
 
         Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start()
         {
+            actionScheduler = GetComponent<ActionScheduler>();
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
             mover = GetComponent<Mover>();
@@ -32,13 +36,36 @@ namespace RPG.Control
 
             if (GetPlayerIsInRange(player) && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                mover.StartMoveAction(guardPosition);
+                GuardBehaviour();
             }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            actionScheduler.CancelCurrentAction();
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+
         private bool GetPlayerIsInRange(GameObject player)
         {
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
