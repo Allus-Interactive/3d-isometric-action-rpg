e3d87694f6574806cdfdc6d2432a6805776f16e8
Reece <reecemorgandev@gmail.com>
2023-01-05 14:52:13 +0000

    visualize Patrol Paths

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float waypointRadius = 0.3f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextWaypoint(i);
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(GetWaypoint(i), waypointRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        private int GetNextWaypoint(int i)
+        {
+            if (i + 1 == transform.childCount) return 0;
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
